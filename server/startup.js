fdd6bf2d5ea20cb13c7b666cc4e7d65b52112eee
oogre <vincent@ogre.be>
2015-02-25 01:39:16 +0100

    arduino part

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,9 @@
 /*jshint strict : false */
 /*global Meteor : false */
-/*global JOKES : false */
 /*global Readers : false */
+/*global Cylon : false */
+/*global console : false */
+/*global Npm : false */
 
 var resetReaders = function(){
 	Readers.remove({});
@@ -15,8 +17,106 @@ var resetReaders = function(){
 		id : 22,
 		reading : false
 	});
-}
+};
+
+
+var buttonTimeout = {
+	warm : undefined,
+	cold : undefined
+};
+var initCylon = Meteor.bindEnvironment(function(){
+	
+	Cylon.robot({
+		name: "Number Six",
+		description: "Description is optional...",
+
+		connections: {
+			arduino: { 
+				adaptor: "firmata", 
+				port: "/dev/tty.usbmodem1411" 
+			}
+		},
+
+		devices: {
+			warm: { 
+				driver: "button", 
+				pin: 8
+			},
+			cold: { 
+				driver: "button", 
+				pin: 9
+			}
+		},
+		work: function (my) {
+			var Fiber = Npm.require("fibers");
+			my.warm.on("push", function(){
+				Fiber(function() { 
+					Meteor.clearTimeout(buttonTimeout.warm);
+					buttonTimeout.warm = Meteor.setTimeout(function() {
+						console.log("bruno ON");
+						Readers.update({
+							name : "bruno"
+						},
+						{
+							$set : {
+								reading : true
+							}
+						});
+					}, 50);
+				}).run();
+			});
+			my.warm.on("release", function(){
+				Fiber(function() { 
+					Meteor.clearTimeout(buttonTimeout.warm);
+					buttonTimeout.warm = Meteor.setTimeout(function() {
+						console.log("bruno OFF");
+						Readers.update({
+							name : "bruno"
+						},
+						{
+							$set : {
+								reading : false
+							}
+						});
+					}, 50);
+				}).run();
+			});
+			my.cold.on("push", function(){
+				Fiber(function() { 
+					Meteor.clearTimeout(buttonTimeout.cold);
+					buttonTimeout.cold = Meteor.setTimeout(function() {
+						console.log("juliette ON");
+						Readers.update({
+							name : "juliette"
+						},
+						{
+							$set : {
+								reading : true
+							}
+						});
+					}, 50);
+				}).run();
+			});
+			my.cold.on("release", function(){
+				Fiber(function() { 
+					Meteor.clearTimeout(buttonTimeout.cold);
+					buttonTimeout.cold = Meteor.setTimeout(function() {
+						console.log("juliette OFF");
+						Readers.update({
+							name : "juliette"
+						},
+						{
+							$set : {
+								reading : false
+							}
+						});
+					}, 50);
+				}).run();
+			});
+		}
+	}).start();
+});
 
 Meteor.startup(function () {
-
+	initCylon();
 });
